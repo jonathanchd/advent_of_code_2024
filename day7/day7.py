cbd9e48f5f82ba4ae10b7bd78e7b167e2b6e1b00
Jonathan <jhchen360@gmail.com>
2024-12-07 02:25:50 -0500

    day7

---
+++ day7/day7.py
@@ -0,0 +1,28 @@
+import sys
+import math
+infile = sys.argv[1]
+D = open(infile).read().strip()
+
+lines = D.split('\n')
+
+ans = 0
+
+def search(index, curr, target, nums):
+    if curr > target:
+        return False
+    if index == len(nums) - 1:
+        if curr == target:
+            return True
+        return False
+    return search(index + 1, curr * nums[index + 1], target, nums) or search(index + 1, curr + nums[index + 1], target, nums)
+
+for line in lines:
+    line = line.split(":")
+    target = int(line[0])
+    nums = list(map(int, line[1].strip().split(" ")))
+    if math.prod(nums) < target:
+        continue
+    if search(0, nums[0], target, nums):
+        ans += target
+
+print(ans)
